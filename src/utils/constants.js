9a77d23bc214075a14cf4498f2b5ccace915ac13
Benson Osei-Mensah <kbenson643@gmail.com>
2023-08-18 21:00:24 +0000

    fix 1.0
    Signed-off-by: Benson Osei-Mensah <kbenson643@gmail.com>

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,6 +1,6 @@
 export const APP_NAME = "Rec.AI";
 const API_URL = "https://recai.onrender.com/api";
-// const API_URL = "http://localhost:3000/api"
+// const API_URL = "http://localhost:3000/api";
 
 export const RECOMMENDATION_ENDPOINT = API_URL + '/query';
 export const SONG_DETAILS_ENDPOINT = API_URL + '/query/song';
